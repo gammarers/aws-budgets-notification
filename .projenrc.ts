fa26bb34981c9e9c4b3e97c2eaa9f9ed9e40a902
yicr <yicr@users.noreply.github.com>
2026-01-02 07:34:06 +0000

    chore(deps): update cdk version to 2.189.1

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,8 +2,7 @@ import { awscdk, javascript } from 'projen';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'yicr',
   authorAddress: 'yicr@users.noreply.github.com',
-  cdkVersion: '2.80.0',
-  constructsVersion: '10.0.5',
+  cdkVersion: '2.189.1',
   typescriptVersion: '5.3.x',
   jsiiVersion: '5.3.x',
   defaultReleaseBranch: 'main',
